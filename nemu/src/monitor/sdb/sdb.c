40c3607f53b288b1231bc955f02c55a25d5aa048
tracer-ics2025 <tracer@njuics.org>
2026-06-01 21:08:39 +0800

    >  compile NEMU 20252114788 WCY Linux ubuntu 5.4.0-150-generic #167~18.04.1-Ubuntu SMP Wed May 24 00:51:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:08:39 up  2:43,  1 user,  load average: 0.34, 0.26, 0.20

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -54,7 +54,7 @@ static int cmd_c(char *args)
 
 static int cmd_q(char *args) { return -1; }
 
-static int cmd_si(char *args) {return -1; }
+static int cmd_si(char *args) { return -1; }
 
 static int cmd_help(char *args);
 
@@ -67,7 +67,7 @@ static struct
     {"help", "Display information about all supported commands", cmd_help},
     {"c", "Continue the execution of the program", cmd_c},
     {"q", "Exit NEMU", cmd_q},
-    {"si [N]", "Execute N instructions. If N is not given, execute 1. ",cmd_si},
+    {"si", "Execute N instructions. If N is not given, execute 1. ",cmd_si},
 
     /* TODO: Add more commands */
 
