5209935c753e4356b07e28cd72801a8d991047b2
tracer-ics2025 <tracer@njuics.org>
2026-06-11 00:16:32 +0800

    >  compile NEMU 20252114788 WCY Linux ubuntu 5.4.0-150-generic #167~18.04.1-Ubuntu SMP Wed May 24 00:51:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  00:16:32 up  3:21,  1 user,  load average: 0.43, 0.12, 0.07

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -12,7 +12,6 @@
  *
  * See the Mulan PSL v2 for more details.
  ***************************************************************************************/
-
 #include <isa.h>
 
 /* We use the POSIX regex functions to process regular expressions.
