3e4b13c3213ff40d3c1a508431e940fc0636008e
wcy <3112770278@qq.com>
2026-06-12 11:10:28 +0800

    expr

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -31,39 +31,93 @@ static char *code_format =
 "  return 0; "
 "}";
 
+static int idx = 0;  // 当前写入位置
+
+// 生成随机数字 (0-99)
+static void gen_num() {
+    int num = rand() % 100;
+    idx += sprintf(buf + idx, "%d", num);
+}
+
+// 生成随机运算符
+static void gen_rand_op() {
+    char op;
+    switch (rand() % 4) {
+        case 0: op = '+'; break;
+        case 1: op = '-'; break;
+        case 2: op = '*'; break;
+        default: op = '/'; break;
+    }
+    buf[idx++] = op;
+}
+
+// 随机插入空格 (30% 概率)
+static void maybe_insert_space() {
+    if (rand() % 100 < 30) {
+        buf[idx++] = ' ';
+    }
+}
+
+// 生成随机表达式
 static void gen_rand_expr() {
-  buf[0] = '\0';
+    int choice = rand() % 3;
+    
+    if (choice == 0) {
+        // 数字
+        gen_num();
+        maybe_insert_space();
+    } 
+    else if (choice == 1) {
+        // 括号表达式
+        buf[idx++] = '(';
+        maybe_insert_space();
+        gen_rand_expr();
+        maybe_insert_space();
+        buf[idx++] = ')';
+        maybe_insert_space();
+    } 
+    else {
+        // 二元运算
+        gen_rand_expr();
+        maybe_insert_space();
+        gen_rand_op();
+        maybe_insert_space();
+        gen_rand_expr();
+    }
 }
 
 int main(int argc, char *argv[]) {
-  int seed = time(0);
-  srand(seed);
-  int loop = 1;
-  if (argc > 1) {
-    sscanf(argv[1], "%d", &loop);
-  }
-  int i;
-  for (i = 0; i < loop; i ++) {
-    gen_rand_expr();
-
-    sprintf(code_buf, code_format, buf);
-
-    FILE *fp = fopen("/tmp/.code.c", "w");
-    assert(fp != NULL);
-    fputs(code_buf, fp);
-    fclose(fp);
-
-    int ret = system("gcc /tmp/.code.c -o /tmp/.expr");
-    if (ret != 0) continue;
-
-    fp = popen("/tmp/.expr", "r");
-    assert(fp != NULL);
-
-    int result;
-    ret = fscanf(fp, "%d", &result);
-    pclose(fp);
-
-    printf("%u %s\n", result, buf);
-  }
-  return 0;
+    int seed = time(0);
+    srand(seed);
+    int loop = 1;
+    if (argc > 1) {
+        sscanf(argv[1], "%d", &loop);
+    }
+    
+    for (int i = 0; i < loop; i++) {
+        idx = 0;
+        gen_rand_expr();
+        buf[idx] = '\0';  // 添加字符串结束符
+        
+        sprintf(code_buf, code_format, buf);
+        
+        FILE *fp = fopen("/tmp/.code.c", "w");
+        assert(fp != NULL);
+        fputs(code_buf, fp);
+        fclose(fp);
+        
+        int ret = system("gcc /tmp/.code.c -o /tmp/.expr");
+        if (ret != 0) continue;
+        
+        fp = popen("/tmp/.expr", "r");
+        assert(fp != NULL);
+        
+        int result;
+        ret = fscanf(fp, "%d", &result);
+        pclose(fp);
+        
+        printf("%u %s\n", result, buf);
+    }
+    
+    return 0;
 }
